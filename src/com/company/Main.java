b14c5baadc033b777abdf8b7bef28c52a7c2dcc4
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-06 10:53:17 -0400

    Worked on Relational operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,25 +18,55 @@ public class Main {
 	    ! - NOT
 	 */
 
-	int a = 2;
-	int b = 4;
-	int x = 6;
-	int y = 1;
+        int a = 2;
+        int b = 4;
+        int x = 6;
+        int y = 1;
 
-	System.out.println("a = 2");
-	System.out.println("b = 4");
-	System.out.println("x = 6");
-	System.out.println("y = 1");
+        System.out.println("a = 2");
+        System.out.println("b = 4");
+        System.out.println("x = 6");
+        System.out.println("y = 1");
 
 //AND Operator
-        if (a == x && y + y == a){
-            System.out.println ("a == x && y + y == a")
-          // OR Operator
-         if (a == x || y + y == a)
-             System.out.println ("a == x || y + y ==a")
-          \*! operator
-            if (a != x && y + y == a)
-                System.out.println ("a != x && y + y == a ")
+        if (a == x && y + y == a) {
+            System.out.println("a == x && y + y == a");
+        }
+        // OR Operator
+        if (a == x || y + y == a) {
+            System.out.println("a == x || y + y ==a");
+        }
+        //NOT Operator
+        if (a != x && y + y == a) {
+            System.out.println("a != x && y + y == a ");
+        }
+        //Less than <
+        if (a < 4) {
+            System.out.println("a is less than 6");
+        }
+        //Less than OR equal to
+        if (a <= 2) {
+            System.out.println("2 is less than OR equal to a");
+        }
+        if (a > 7) {
+            System.out.println("a is greater than 7");
+        }
+        if (a >= 8) {
+            System.out.println("a is greater than or equal to 8");
+        }
+        if (a == 2) {
+            System.out.println("a equals 2");
+        }
+        if (a != 5) {
+            System.out.println("a does not equal 5");
         }
     }
 }
+
+
+
+
+
+
+
+
